7aa2ff33649f184fbba3f41f078db189ea21d172
MohanKumarMP <mohankumar.mp@hudupu.com>
2024-08-24 02:13:12 +0530

    code restored

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
-const expressApp = express();
-
-async function createNestServer() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-  app.enableCors(); // Enable CORS if needed
-  await app.init();
-  return expressApp;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-
-let cachedServer: express.Express;
-
-export default async function handler(
-  req: express.Request,
-  res: express.Response,
-) {
-  if (!cachedServer) {
-    cachedServer = await createNestServer();
-  }
-  return cachedServer(req, res);
-}
+bootstrap();
