6d3521fe17a3e7cff5461841d8b176b54a8f6ff5
MohanKumarMP <mohankumar.mp@hudupu.com>
2024-08-23 22:57:14 +0530

    port updated

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.NESTJS_PORT || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
