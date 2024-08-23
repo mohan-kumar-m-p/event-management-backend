b23d3a0dd86d2d4f81c342c7bf0f51abeae661bf
MohanKumarMP <mohankumar.mp@hudupu.com>
2024-08-23 23:45:52 +0530

    vercel file updated

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+console.log('Starting NestJS application...');
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Bootstrap function entered.');
   const app = await NestFactory.create(AppModule);
+  console.log('NestFactory created.');
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+bootstrap().catch(err => console.error('Error during bootstrap:', err));
