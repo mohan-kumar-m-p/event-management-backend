8a02d0a28ed703f7f212323289844d8b617720b9
Cartrogen <nishilgosalia@gmail.com>
2024-09-16 17:09:17 -0400

    CORs

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,12 @@ import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: ['https://www.pssemrevents.com', 'http://localhost:3000'],
-  //   methods: 'GET,POST,PUT,PATCH,DELETE',
-  //   allowedHeaders: 'Content-Type, Authorization',
-  //   credentials: true,
-  // });
-  app.enableCors({ origin: '*' })
+  app.enableCors({
+    origin: ['https://www.pssemrevents.com', 'http://localhost:3000'],
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
